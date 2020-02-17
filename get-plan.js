b8da47ff964db079365464a15d66aee5b0459784
der-bingle <luke.m.wilson@gmail.com>
2020-02-17 13:14:37 -0600

    Get time from config instead of hardcoded

--- get-plan.js
+++ get-plan.js
@@ -1,28 +1,40 @@
-const startOfWeek = require('date-fns/fp/startOfWeek');
+const R = require("ramda");
+const set = require('date-fns/fp/set');
+const config = require("./config")
+const format = require('date-fns/fp/format');
 const addWeeks = require('date-fns/fp/addWeeks');
 const isSunday = require('date-fns/fp/isSunday');
-const formatISO = require('date-fns/fp/formatISO');
-const format = require('date-fns/fp/format');
-const set = require('date-fns/fp/set');
+const startOfWeek = require('date-fns/fp/startOfWeek');
 
-const setTime = set({
-  hours: 10,
-  minutes: 40,
-  seconds: 00
-})
+let now = new Date();
+let dateToString = format("R-MM-dd'T'H:mm:ss'Z'")
 
-let isoFormat = format("R-MM-dd'T'H:mm:ss'Z'")
-// let nextSunday = 
-let getSunday = () => {
-  // let today = setTime(new Date());
-  let today = setTime(new Date(2020, 02, 18));
-  if (isSunday(today)) {
-   return isoFormat(today); 
+const serviceTime = (date) => {
+  let time = config.get("serviceTime").split(":")
+  let hours = parseInt(time[0]);
+  let minutes = parseInt(time[1]);
+  return set({
+    hours,
+    minutes,
+    seconds: 00
+  }, date)
+};
+
+
+let getNextSunday = R.pipe(
+  addWeeks(1),
+  startOfWeek,
+  serviceTime,
+  dateToString
+);
+
+let getNextService = () => {
+  if (isSunday(now)) {
+    return isoFormat(now);
   } else {
-    return isoFormat(setTime(startOfWeek(addWeeks(today, 1))))
+    return getNextSunday(now)
   }
 }
-let string = 'YYYY-MM-DDTHH:MM:SSZ'
-let example = "2020-02-23T10:40:00Z";
 
-console.log(getSunday());
+let nextService = getNextService();
+console.log(sunday);
