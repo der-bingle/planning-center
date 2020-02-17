93f7213a1440d3a57fd39dc162d483fdf550a7be
der-bingle <luke.m.wilson@gmail.com>
2020-02-17 15:17:28 -0600

    Map response to expected structure

--- get-plan.js
+++ get-plan.js
@@ -1,4 +1,5 @@
 const R = require("ramda");
+const got = require("got");
 const set = require('date-fns/fp/set');
 const config = require("./config")
 const format = require('date-fns/fp/format');
@@ -20,7 +21,6 @@ const serviceTime = (date) => {
   }, date)
 };
 
-
 let getNextSunday = R.pipe(
   addWeeks(1),
   startOfWeek,
@@ -28,13 +28,63 @@ let getNextSunday = R.pipe(
   dateToString
 );
 
-let getNextService = () => {
+let getNextServiceDate = () => {
   if (isSunday(now)) {
     return isoFormat(now);
   } else {
     return getNextSunday(now)
   }
 }
+let nextServiceDate = getNextServiceDate();
 
-let nextService = getNextService();
-console.log(sunday);
+let gotOpts = {
+  // hostname: "api.planningcenteronline.com",
+  username: config.get("appId"),
+  password: config.get("secret"),
+  responseType: "json",
+  resolveBodyOnly: true
+}
+
+let isNextService = R.pipe(
+  R.prop("attributes"),
+  R.propEq("sort_date", nextServiceDate)
+);
+
+let getPlanId = R.pipe(
+  R.prop("data"),
+  R.find(isNextService),
+  R.prop("id"),
+  // R.prop("links"),
+  // R.prop("self"),
+);
+
+let serviceTypeID = config.get("serviceTypeID")
+
+let getNextPlanID = async () => {
+  gotOpts.url = `https://api.planningcenteronline.com/services/v2/service_types/${serviceTypeID}/plans`
+  gotOpts.searchParams = {filter: "future"}
+  let planList = await got(gotOpts);
+  // console.log(got)
+  return getPlanId(planList);
+}
+
+let getNextPlan = async (planID) => {
+  gotOpts.url = `https://api.planningcenteronline.com/services/v2/service_types/${serviceTypeID}/plans/${planID}/items`
+  gotOpts.searchParams = {include: "arrangement"}
+  let plan = await got(gotOpts);
+  return plan.included.map(mapSongs);
+}
+
+let mapSongs = song => ({
+  order: song.attributes.sequence,
+  rawLyrics: song.attributes.lyrics
+})
+
+let main = async () => {
+  let planID = await getNextPlanID();
+  // console.log(planID)
+  let plan = await getNextPlan(planID);
+  console.log(JSON.stringify(plan, null, 2))
+}
+
+main().catch(error => console.log(error))
