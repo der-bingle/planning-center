4d812c864ab4e3a805ce0714c506c93659fa5ba6
der-bingle <59025084+der-bingle@users.noreply.github.com>
2020-02-18 22:41:31 -0600

    Add smart repetition of slides (dedup if single slide, not if multiple.)

--- source/songs.js
+++ source/songs.js
@@ -8,10 +8,24 @@ let newLineJoin = R.join("\n\n");
 
 let addBlank = R.append(script.newSlide("Blank"))
 
+const l = [1, -1, 1, 3, 4, -4, -4, -5, 5, 3, 3];
+R.dropRepeatsWith(R.eqBy(Math.abs), l);
+
+
 let makeOneSong = (song) => {
   let lyrics = getLyrics(song)
   let sequence = getSequence(song)
-
+  
+  let isEqual = (part1, part2) => {
+    let part1Lyrics = lyrics.get(part1);
+    if (part1 === part2 && part1Lyrics.length === 1) {
+      return true
+    } else {
+      return false
+    }
+  }
+ 
+  let dedupSequence = R.dropRepeatsWith(isEqual, sequence);
   let addSection = (script, section) => {
     let sectionSlides = lyrics.get(section)
     let newScript = sectionSlides.map(slide => makeSlides.lyrics(slide))
@@ -23,7 +37,7 @@ let makeOneSong = (song) => {
     addBlank,
     newLineJoin
   );
-  return make(sequence)
+  return make(dedupSequence)
 }
 
 let makeSongs = list => newLineJoin(list.map(song => makeOneSong(song)));
